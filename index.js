3c0f351b59015961b8e5a5fb4ec9e6ccbe92876e
Prince04012004 <princekudiya2004@gmail.com>
2026-03-03 12:21:39 +0530

    Backend: Fixed Port and CORS for Render deployment

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import authroutes from "./routes/authroutes.js";
-import addexpenseRouter  from "./routes/expenseroutes.js";
+import addexpenseRouter from "./routes/expenseroutes.js";
 import cors from "cors";
 
-
-const port = 3000;
-
+const port = process.env.PORT || 3000; 
 
 const app = express();
+
 app.use(cors());
-
-
-// 1. Database connection
-connectDB();
-
-// 2. Middleware (MUST come before routes)
 app.use(express.json()); 
 
-// 3. Routes
+connectDB();
+
 app.use('/api', authroutes);
-app.use('/api', addexpenseRouter)
+app.use('/api', addexpenseRouter);
 
 app.get("/", (req, res) => {
-  res.send("Hello ji");
-  console.log("API is running");
+  res.send("BudgetBox Backend is Running!");
 });
 
 app.listen(port, () => {
