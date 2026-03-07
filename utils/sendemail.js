17371fc658da7edd53ba46f24712c7944ebca900
Prince04012004 <princekudiya2004@gmail.com>
2026-03-07 22:27:17 +0530

    fix gmail smtp ipv6 issue

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -1,17 +1,18 @@
-import nodemailer from "nodemailer";
 import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
-
-dns.setDefaultResultOrder("ipv4first");
 
 export const sendmail = async (email, otp) => {
   try {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
+      family: 4, // force IPv4
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
@@ -23,20 +24,23 @@ export const sendmail = async (email, otp) => {
       to: email,
       subject: "Verification Code - BudgetBox",
       html: `
-      <div style="font-family: Arial, sans-serif; padding: 20px;">
-      <h2>BudgetBox Verification</h2>
-      <p>Your OTP code is <b>${otp}</b></p>
+      <div style="font-family: Arial; padding:20px;">
+        <h2>BudgetBox Verification</h2>
+        <p>Your OTP is <b>${otp}</b></p>
+        <p>This code will expire in 10 minutes.</p>
       </div>
       `
     };
 
     const info = await transporter.sendMail(mailoptions);
 
-    console.log("Email sent:", info.messageId);
+    console.log("Email sent successfully:", info.messageId);
     return info;
 
   } catch (err) {
+
     console.error("FULL ERROR:", err);
     throw new Error("Failed to send email");
+
   }
 };
