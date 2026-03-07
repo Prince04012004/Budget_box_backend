058cd2852532df96d969e1d4b1f28d0f15863937
Prince04012004 <princekudiya2004@gmail.com>
2026-03-07 15:14:01 +0530

    fix: force ipv4 to resolve ENETUNREACH on render

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -5,20 +5,19 @@ dotenv.config();
 export const sendmail = async (email, otp) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "74.125.200.108", 
-      port: 465,
-      secure: true,
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER, 
         pass: process.env.EMAIL_PASS,
       },
       family: 4,
-      connectionTimeout: 20000,
-      greetingTimeout: 20000,
-      socketTimeout: 20000,
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
       tls: {
-        rejectUnauthorized: false,
-        servername: "smtp.gmail.com"
+        rejectUnauthorized: false
       }
     });
 
