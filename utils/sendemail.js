20e5c0bf23a4805e3356fa4759ddf9a7092e9645
Prince04012004 <princekudiya2004@gmail.com>
2026-03-09 20:26:13 +0530

    Updated sendmail function with EmailJS

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -2,20 +2,19 @@ export const sendmail = async (email, otp) => {
   const data = {
     service_id: process.env.EMAILJS_SERVICE_ID,
     template_id: process.env.EMAILJS_TEMPLATE_ID,
-    user_id: process.env.EMAILJS_PUBLIC_KEY,
-    private_key: process.env.EMAILJS_PRIVATE_KEY, 
+    user_id: process.env.EMAILJS_PUBLIC_KEY, // EmailJS ke liye public key hi use hoti hai
     template_params: {
-      to_email: email, // Aapke template mein {{to_email}}
-      passcode: otp,   // Aapke template mein {{passcode}}
-      time: new Date().toLocaleTimeString(), // 👈 Ye line add kardo kyunki template mein {{time}} hai
+      to_email: email,        // Template me {{to_email}}
+      passcode: otp,          // Template me {{passcode}}
+      time: new Date().toLocaleTimeString(), // Agar template me {{time}} hai
     },
   };
 
   try {
-    const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
-      method: 'POST',
+    const response = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
